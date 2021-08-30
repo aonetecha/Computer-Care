29b781bace91d426561bdc99599f662d8d16fcea
aonetech <aonetech@code.com>
2021-08-30 00:16:03 -0700

    ok

---
+++ Computer Care.bat
@@ -0,0 +1,357 @@
+
+    :: Version - 1.0.0
+    ::Founder - Anindya Ghoshal
+
+
+@echo off 
+rename ffmpeg-4.4-full_build ffmpeg
+move ffmpeg C:\
+cls
+:Care
+color 70
+echo.
+echo.
+echo                                            ########################### 
+echo                                            #                         #     
+echo                                            #    [=Computer Care=]    #   
+echo                                            #                         #
+echo                                            ###########################
+echo.
+echo.
+echo  Press [1] to Clean Junk file...                                               Press [10] to DtatRecover...                                              
+echo.                                                                                                     
+echo  Press [2] to disk Format...                                                   Press [11] to Computer Activity View...                                                                                    
+echo. 
+echo  Press [3] to File Search...                                                   Press [12] to Create Hostpot in your Computer...                                                                                                    
+echo.
+echo  Press [4] to disk Optimization...                                             Press [13] to Create Bootable pendrive...                                                                                                     
+echo.
+echo  Press [5] to Fix logical error [and] Repair disk bad sector...                Press [14] to Test Internet Speed...                             
+echo.
+echo  Press [6] to Data Delete permanetly [Data cannot be RECOVERED]                Press [15] to Screnn Recoder...                                                  
+echo.
+echo  Press [7] to Background process...                                            Press [16] to View USB Activity...                                           
+echo.
+echo  Press [8] to System Information...                                            Press [h] to Help... / Press [e] to Exit...                                             
+echo.
+echo  Press [9] Protect File and Foleder...
+echo. 
+set /p var="What you want chosse => " 
+echo.
+if %var% == 1 goto a 
+if %var% == 2 goto b
+if %var% == 3 goto c
+if %var% == 4 goto d
+if %var% == 5 goto e
+if %var% == 6 goto f
+if %var% == 7 goto g
+if %var% == 8 goto h
+if %var% == 9 goto i
+if %var% == 10 goto j
+if %var% == 11 goto k
+if %var% == 12 goto l
+if %var% == 13 goto m 
+if %var% == 14 goto n
+if %var% == 15 goto o
+if %var% == 16 goto p
+if %var% == h goto mainhelp
+if %var% == e goto exit
+cls
+goto Care
+:a
+echo.  
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q c:\windows\prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windwos\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\temp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+cls
+echo.
+pause
+cls
+goto Care
+
+:b
+echo.
+set /p disk="Enter Drive letter =>"
+format /q /x %disk%:
+echo.
+echo.
+pause
+cls
+goto Care
+
+:c
+:go
+cls
+echo.
+echo   Press [1] to Search...
+echo.
+echo   Write [2] to Help...
+echo.
+echo   Write [3] to Back...
+echo.
+set /p n="What you want chosse => "
+if %n% == 1 goto a
+if %n% == 2 goto help
+if %n% == 3 goto back
+pause
+:a
+echo.
+set /p drive="Enter drive letter =>"
+echo.
+set /p file="Enter file or folder name =>"
+where /r %drive%: *%file%*
+echo.
+pause
+goto go
+:help
+echo.
+echo  First you have to type the drive letter in the drive that will search the file and
+echo.
+echo  After that you have to give the file or folder name..
+echo.
+echo Example :-
+echo                        Enter drive letter = D
+echo                        Enter file or folder name = help.txt or .txt
+echo.            
+pause
+cls
+goto go
+:back
+cls
+goto Care
+
+:d
+echo.
+set /p disk3="Enter drive letter =>"
+defrag %disk3%: /u
+pause
+cls
+goto Care
+exit /d
+
+:e
+echo.
+set /p disk2="Enter dirve letter =>"
+chkdsk /f /r %disk2%:
+echo.
+pause
+cls
+goto Care
+
+:f
+cls
+echo.
+echo        Warning!!!
+echo.
+echo  Data cannot be RECOVERED###...
+echo.
+set /p disk4="Enter drive letter =>"
+echo.
+set /p disk5="Enter file system =>"
+echo.
+format %disk4%: /fs:%disk5% /p:1
+cls
+pause
+goto Care
+:g
+netstat -ao
+netstat -f
+pause
+cls
+goto Care
+:h
+echo.
+systeminfo
+echo.
+pause
+cls 
+goto Care
+
+:i
+:prot
+cls
+echo  Press [1] ON File or Folder Protection...
+echo.
+echo  Press [2] OFF File or Folder Protection...
+echo. 
+echo  Press [3] to Back...
+echo.
+set /p x="What you want chosse => "
+if %x% == 1 goto on
+if %x% == 2 goto off
+if %x% == 3 goto back
+cls
+
+:on
+echo. 
+set /p file="Enter drive letter =>"
+echo.
+set /p file2="Enter File or Folder name =>"
+%file%:
+cacls "%file2%" /p everyone:n
+pause
+cls
+goto prot  
+:off
+echo.
+set /p file="Enter drive letter =>"
+echo.
+set /p file2="Enter File or Folder name =>"
+%file%:
+cacls "%file2%" /p everyone:f
+pause
+cls
+goto prot
+:back
+cls
+goto Care
+:j
+cd DataRecover
+START DataRecover.exe
+echo.
+pause
+exit
+
+:k
+cd ComputerActivity
+START ComputerActivity.exe
+echo.
+pause
+exit
+
+:l
+cls
+:HOST
+echo.
+echo  Press [1] On Hostpot...
+echo.
+echo  Press [2] OFF Hostpot...
+echo.
+echo  Press [3] to Help...
+echo.
+echo  Press [4] to Back...
+echo.
+set /p a="What Want do Chosse =>"
+if %a% == 1 goto on
+if %a% == 2 goto off
+if %a% == 3 goto he
+if %a% == 4 goto e
+:on
+echo.
+set /p net="Enter SSID => "
+echo.
+set /p net2="Enter Password => "
+netsh wlan set hostednetwork mode=allow ssid="%net%" key="%net2%"
+netsh wlan start hostednetwork
+pause
+cls
+goto HOST
+:off
+netsh wlan stop hostednetwork
+pause
+cls
+goto HOST
+:he
+echo.
+echo                           [Must be run the Run as administrator]
+echo.
+echo.
+echo   First you need to use it to check if your computer has facility to Open cmd and type it :- netsh wlan show driver 
+echo.
+echo   [Hosted Network Supported : Yes] If Yes then you can use this feature and
+echo.
+echo   [Hosted Supported : No] If no then wifi adapter needs to be upgraded...
+echo. 
+pause
+cls
+goto HOST
+:e
+cls
+goto Care
+
+:n
+cd speedtest
+START speedtest.exe
+echo.
+pause
+exit /b
+
+:m 
+cd Bootable pendrive
+START rufus.exe
+echo.
+pause 
+exit 
+
+:o
+mkdir ScreenRecord
+cls 
+echo.
+echo   [It will not Open run as administrator doing this]
+echo.
+echo  Press q to turn off screen record.
+echo.
+echo.
+set /p scr=" Enter Your output File name => "
+ffmpeg -f gdigrab -framerate 60 -i desktop "ScreenRecord\%scr%.mp4"
+echo.
+pause
+cls
+goto Care
+
+:p
+cd usb 
+START USBDeview.exe 
+echo.
+pause
+cls
+exit /b
+ 
+:mainhelp
+cls
+echo.
+echo.
+
+echo            #     #   #######   #          #########    
+echo            #     #   #         #          #       #
+echo            #     #   #         #          #       #
+echo            #######   #######   #          #########
+echo            #     #   #         #          #
+echo            #     #   #         #          #
+echo            #     #   #######   #########  #   
+
+
+echo.
+echo  [This application will must be run the Run as administrator]
+echo.
+echo    But when recovering data and viewing computer activity and Create Bootable pendrive and 
+echo.
+echo    View USB Activity,this application will NOT open run as administrator.
+echo. 
+echo      Thank You For Download :)
+echo.
+pause
+cls
+goto Care
+:exit
+exit /b
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# Computer-Care
+Download Now
+
+![1001](https://user-images.githubusercontent.com/85815644/131119694-f8c52bc9-b30d-4983-b448-f0d526903c9c.gif)
+
+![Capture2](https://user-images.githubusercontent.com/85815644/131119606-66058a30-e527-4fac-a698-695a4cbeca51.PNG)
+
+![1](https://user-images.githubusercontent.com/85815644/131119741-007bbb4e-c989-487d-ba06-a966ad81f0c0.PNG)
+
+![55](https://user-images.githubusercontent.com/85815644/131119972-269a6bce-d919-4919-b945-44f310fc97cf.PNG)
